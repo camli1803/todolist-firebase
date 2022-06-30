69193be93baaf2470f41f08d118f5e816784256b
Nguyen Thi Cam Li <linguyen@gmail.com>
2022-06-30 23:24:44 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxBiq__YdYXTwSpE5my9s0uJTof3IkSGY",
+  authDomain: "fire-base-sample-53ec0.firebaseapp.com",
+  projectId: "fire-base-sample-53ec0",
+  storageBucket: "fire-base-sample-53ec0.appspot.com",
+  messagingSenderId: "902624370973",
+  appId: "1:902624370973:web:ec997620c18e488fbf01f4"
+};
+
+firebase.initializeApp(firebaseConfig);
